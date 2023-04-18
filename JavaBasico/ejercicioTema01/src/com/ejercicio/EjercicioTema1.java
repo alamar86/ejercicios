d4bf2c791f0cb4ece827024aedf403b9337d2d47
Jose Alamar Ferrer <alamar46@gmail.com>
2023-04-18 17:07:01 +0200

    Ejercicio java tema1

---
+++ JavaBasico/ejercicioTema01/src/com/ejercicio/EjercicioTema1.java
@@ -0,0 +1,18 @@
+package com.ejercicio;
+
+public class EjercicioTema1 {
+
+    public static void main(String[] args) {
+        int numeroEntero = 16;
+        long numeroLong = 36L;
+        double numeroDouble = 3.1415926535897;
+        boolean verdaderoFalso = true;
+        String cadena = "Hola Mundo";
+
+        System.out.println("El numero entero es: " + numeroEntero);
+        System.out.println("El numero long es: " + numeroLong);
+        System.out.println("El numero double es: " + numeroDouble);
+        System.out.println("Verdadero falso es: " + verdaderoFalso);
+        System.out.println("Cadena es: " + cadena);
+    }
+}

4ac02bbb513e66cf466bcf600516b7b0f65fef61
Jose Alamar Ferrer <alamar46@gmail.com>
2023-05-02 16:02:56 +0200

    Java avanzado EjercicioTema14 Parte02

--- JavaBasico/EjercicioTema14/EjercicioTema14Parte01/src/nombrado/Funciones.java
+++ JavaBasico/EjercicioTema14/EjercicioTema14Parte01/src/nombrado/Funciones.java
@@ -1,7 +1,18 @@
 package nombrado;
 
+
 import java.util.ArrayList;
 
+/**
+ * Ejercicio correspondiente al Tema 14 del curso de Java Avanzado de operbootcamp
+ * El ejercicio consta de dos parte, una en diseñar dos funciones limpias en función de las normas de nombrado en el
+ * tema 12 y la segunda parte consiste en comentar algun proyecto realizado anteriormente y comentar si hemos detectado
+ * algun fallo en los comentarios.
+ * He decidido realizar las dos partes en una, he decidido comentar con javadoc las dos funciones de la parte 1 del
+ * ejercicio.
+ * @see <a href="https://open-bootcamp.com/">...</a>
+ * @author Jose Alamar Ferrer
+ */
 public class Funciones {
     ArrayList<String> nombres = new ArrayList<String>();
 
@@ -10,7 +21,6 @@ public class Funciones {
      * si se lee igual de izuierda a derecha que de derecha a izquierda
      * @param palabra
      * @return - true si se lee igual en ambas direcciones - false de lo contrario
-     * @author Jose Alamar
      */
     public boolean esPalindromo(String palabra){
         String palabraIvertida = "";
@@ -25,7 +35,6 @@ public class Funciones {
      * @param nombres - lista de nombres
      * @param nombre - nombre a localizar en la lista
      * @return - true si lo encuentra en la lista- false de lo contrario
-     * @author Jose Alamar
      */
     public boolean encontrarNombre(ArrayList<String> nombres, String nombre){
         for(String n : nombres) {
